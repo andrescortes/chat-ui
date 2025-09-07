45dfd0e2af7ffb266cf99372a62f267f973ddf9f
Miguel Cortes <andresabyac@gmail.com>
2025-09-07 09:23:21 -0500

    add comprehensive unit tests for DeleteModalComponent and update component test configurations

--- src/app/layout/delete-modal/delete-modal.component.spec.ts
+++ src/app/layout/delete-modal/delete-modal.component.spec.ts
@@ -1,23 +1,86 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from "@angular/router";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { of, throwError } from "rxjs";
+import { AuthService, ChatService } from "../../services";
 
 import { DeleteModalComponent } from './delete-modal.component';
 
 describe('DeleteModalComponent', () => {
   let component: DeleteModalComponent;
   let fixture: ComponentFixture<DeleteModalComponent>;
+  let chatService: ChatService;
+  let toastrService: ToastrService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeleteModalComponent]
+      imports: [DeleteModalComponent, ToastrModule.forRoot()],
+      providers: [
+        ToastrService,
+        Router,
+        ChatService,
+      ],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(DeleteModalComponent);
     component = fixture.componentInstance;
+    chatService = fixture.debugElement.injector.get(ChatService);
+    toastrService = fixture.debugElement.injector.get(ToastrService);
+    router = fixture.debugElement.injector.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('deleteChat', () => {
+    it('should delete the chat', () => {
+      component.chat = {
+        id: '1235',
+        created_at: '',
+        editable: false,
+        sender: '',
+        text: 'test',
+        users: {
+          id: '12345',
+          avatar_url: '',
+          full_name: ''
+        }
+      };
+      const deleteSpy = spyOn(chatService, 'deleteChat').and.returnValue(of(204));
+      const urlSpy = spyOnProperty(router, 'url', 'get').and.returnValue('/chat');
+      const navigateUrlSpy = spyOn(router, 'navigateByUrl').and.returnValue(Promise.resolve(true));
+      const tosterSuccessSpy = spyOn(toastrService, 'success').and.callThrough();
+
+      component.deleteChat();
+      expect(deleteSpy).toHaveBeenCalledOnceWith(component.chat.id);
+      expect(tosterSuccessSpy).toHaveBeenCalled();
+      expect(urlSpy).toHaveBeenCalled();
+      expect(navigateUrlSpy).toHaveBeenCalled();
+    });
+
+    it('should throws an error', () => {
+      component.chat = {
+        id: '1235',
+        created_at: '',
+        editable: false,
+        sender: '',
+        text: 'test',
+        users: {
+          id: '12345',
+          avatar_url: '',
+          full_name: ''
+        }
+      };
+      const deleteSpy = spyOn(chatService, 'deleteChat').and.returnValue(throwError(() => new Error('test')));
+      const tosterSuccessSpy = spyOn(toastrService, 'error').and.callThrough();
+
+      component.deleteChat();
+      expect(deleteSpy).toHaveBeenCalledOnceWith(component.chat.id);
+      expect(tosterSuccessSpy).toHaveBeenCalledWith('test', 'Chat');
+    })
+  })
 });
--- src/app/pages/chat/chat.component.spec.ts
+++ src/app/pages/chat/chat.component.spec.ts
@@ -1,19 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from "@angular/router";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { AuthService, ChatService } from "../../services";
 
 import { ChatComponent } from './chat.component';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
   let fixture: ComponentFixture<ChatComponent>;
+  let chatService: ChatService;
+  let authService: AuthService;
+  let toastrService: ToastrService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ChatComponent]
+      imports: [ChatComponent,ToastrModule.forRoot()],
+      providers: [
+        ToastrService,
+        Router,
+        ChatService,
+        AuthService,
+      ],
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(ChatComponent);
     component = fixture.componentInstance;
+    chatService = fixture.debugElement.injector.get(ChatService);
+    toastrService = fixture.debugElement.injector.get(ToastrService);
+    router = fixture.debugElement.injector.get(Router);
     fixture.detectChanges();
   });
 
